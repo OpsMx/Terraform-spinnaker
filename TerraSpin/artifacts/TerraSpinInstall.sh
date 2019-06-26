213bc83d06245ec56ffb8582adf6bc97ff7cee9a
lalitv92 <lalitv92@gmail.com>
2019-06-26 18:31:46 +0530

    Added installation script

---
+++ TerraSpin/artifacts/TerraSpinInstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+UserPort=$1
+DefaultPort=8090
+CurrentDir="$(pwd)"
+
+echo current dirtory :: $CurrentDir
+echo user port :: $UserPort
+echo default port :: $DefaultPort
+
+IsRunning="$(ps -ef | grep TerraSpin.jar | wc -l)"	 
+
+if [ $IsRunning -gt 1 ]
+then
+    echo TerraSpin service is already running.
+else
+
+		wget -O TerraSpin.jar "https://github.com/OpsMx/Terraform-spinnaker/blob/master/TerraSpin/artifacts/TerraSpin.jar"
+
+		if [ -z "$UserPort" ]
+    then
+        nohup java -Dserver.port=8090 -jar $CurrentDir/TerraSpin.jar 2>&1 &
+    else
+        nohup java -Dserver.port=$UserPort -jar $CurrentDir/TerraSpin.jar 2>&1 &
+    fi
+fi
